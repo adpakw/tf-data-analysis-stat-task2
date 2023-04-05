61558759e9fbd4f35f513c923a7d0ed0774a061f
adpakw <94380412+adpakw@users.noreply.github.com>
2023-04-05 11:52:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,5 +10,4 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     s = np.std(significance_levels, ddof=1)
     SE = s / np.sqrt(len(x))
-    return x.mean() + SE * norm.ppf(1 - alpha / 2), \
-           x.mean() + SE * norm.ppf(alpha / 2)
+    return x.mean() + SE * norm.ppf(1 - alpha / 2), x.mean() + SE * norm.ppf(alpha / 2)
